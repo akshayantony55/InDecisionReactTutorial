a8543e28e7ddd21e861594d871e4dd18f2cf0bbd
Akshay Antony <akshaya@valorem.com>
2020-09-15 23:49:46 +0530

    es 6 arrow functions

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,10 @@
+const square = function (x) {
+    return x*x;
+}
+
+const squareArrow = (x) => {
+    return x*x;
+}
+
+const squareArrow = (x) => x*x;
+// All arrow functions are anonymous. no name
