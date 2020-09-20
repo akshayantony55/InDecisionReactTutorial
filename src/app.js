7bce00bc8abab882b97f9a6389d8939216727777
Akshay Antony <akshaya@valorem.com>
2020-09-20 23:00:44 +0530

    forms and inputs example

--- src/app.js
+++ src/app.js
@@ -1,27 +1,51 @@
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
-var template = (
-  <div>
-    <h1>Indecision App</h1>
-    <p>This is some info</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-);
-var user = {
-  name: "Akshay"
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
 };
-var templateTwo = (
-  <div>
-    <h1>{user.name}</h1>
-    <p>Age: --112</p>
-    <p>Location: Kerala</p>
-  </div>
-);
 
-var appRoot = document.getElementById('app');
+const onFormSubmit = (e) => {
+  // prevent full page refresh 
+  e.preventDefault();
 
-ReactDOM.render(templateTwo, appRoot);
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+render();
